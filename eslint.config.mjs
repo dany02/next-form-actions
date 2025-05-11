e1320c52721a521aa4bbd56990c1db4194f827f3
dana <handany1102@gmail.com>
2025-05-11 23:01:09 +0900

    deployment vercel 5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,10 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-	  "no-unused-vars": "off",
-	  "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",  
+      "@typescript-eslint/no-unused-vars": "off",  
       "@typescript-eslint/no-explicit-any": "off", 
-	  "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
     },
   },
 ];
